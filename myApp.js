68a46ec3b80ddd0f462ab2c8fb003e7c0a404f83
Silpa Johns <CS5979@cloudium.local>
2021-09-16 11:15:27 +0530

    client

--- myApp.js
+++ myApp.js
@@ -49,6 +49,20 @@ app.get("/:word/echo", (req, res) => {
 
 
 
+app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+
+res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
+
+
+
+
+
 
 
 
